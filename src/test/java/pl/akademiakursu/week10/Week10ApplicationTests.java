5419139a5c8b0c905f20139cf48f0ee2a043a090
borysj <jacek.borys@contractors.roche.com>
2019-12-13 18:04:11 +0100

    Testy MVC

--- src/test/java/pl/akademiakursu/week10/Week10ApplicationTests.java
+++ src/test/java/pl/akademiakursu/week10/Week10ApplicationTests.java
@@ -3,7 +3,6 @@ package pl.akademiakursu.week10;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class Week10ApplicationTests {
 
